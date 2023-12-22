e495b5c7d04754325d2d020bf3f896f4e5db1b10
PiotrGrzechnik <ppgrzechnik@gmail.com>
2023-12-22 21:42:08 +0100

    Addd consollog purchaseToken

--- src/views/app-shop-view.ts
+++ src/views/app-shop-view.ts
@@ -3,7 +3,22 @@ import { customElement } from 'lit/decorators.js';
 
 import '../components/app-map/app-map';
 import { sharedStyles } from '../styles/shared-styles';
-import { alertNotifyPrimary } from '../utils/alertsUtils';
+
+interface Price {
+  currency: string;
+  value: string;
+}
+
+
+interface GoogleBillingItem {
+  description: string;
+  iconURLs: [];
+  introductoryPrice: Price;
+  itemId: string;
+  price: Price;
+  title: string;
+  type: string;
+}
 
 @customElement('app-shop-view')
 export class AppShopView extends LitElement {
@@ -16,6 +31,33 @@ export class AppShopView extends LitElement {
     `,
   ];
 
+  async makePurchase( sku: string) {
+      // Define the preferred payment method and item ID
+      const paymentMethods = [{
+          supportedMethods: "https://play.google.com/billing",
+          data: {
+              sku: sku,
+          }
+      }];
+  
+      // The "total" member of the paymentDetails is required by the Payment
+      // Request API, but is not used when using Google Play Billing. We can
+      // set it up with bogus details.
+      const paymentDetails = {
+          total: {
+              label: `Total`,
+              amount: {currency: `USD`, value: `0`}
+          }
+      };
+  
+      const request = new PaymentRequest(paymentMethods, paymentDetails);
+      // try {
+          const paymentResponse = await request.show();
+          const {purchaseToken} = paymentResponse.details;
+          console.log(purchaseToken);
+  // }
+ }
+
 
   protected async firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
     if ('getDigitalGoodsService' in window) {
@@ -24,13 +66,13 @@ export class AppShopView extends LitElement {
         const service = await (window as any).getDigitalGoodsService('https://play.google.com/billing');
         // Google Play Billing is supported!
 
-      const skuDetails = await service.getDetails(['android.test.purchased','doghouse_3_pack', 'shiny_sword']);
-      alertNotifyPrimary(JSON.stringify(skuDetails), {duration: Infinity})
+      const skuDetails: GoogleBillingItem[] = await service.getDetails(['android.test.purchased','doghouse_3_pack']);
       console.log(skuDetails);
 
-      const existingPurchases = await service.listPurchases();
-      console.log(existingPurchases);
-
+      this.makePurchase('android.test.purchased')
+      
+      // const existingPurchases = await service.listPurchases();
+      // console.log(existingPurchases);
       } catch (error) {
         // Google Play Billing is not available. Use another payment flow.
         return;
