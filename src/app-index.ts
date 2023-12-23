ff8c5c1d7af627750c8668077a5201155a6df1c4
PiotrGrzechnik <ppgrzechnik@gmail.com>
2023-12-23 11:57:34 +0100

    Remove check foe premission geoloc

--- src/app-index.ts
+++ src/app-index.ts
@@ -98,7 +98,6 @@ export class AppIndex extends LitElement {
 
   willUpdate(changedProperties: PropertyValueMap<this>) {
     console.log(     this.view === View.LOADING_MAP_VIEW,
-      this.geolocation.permissionGeolocation ,
       this.userPos ,
       this.userInfo);
     if (changedProperties.has('view') && changedProperties.size === 1) {
@@ -107,7 +106,6 @@ export class AppIndex extends LitElement {
 
     if (
       this.view === View.LOADING_MAP_VIEW &&
-      this.geolocation.permissionGeolocation &&
       this.userPos &&
       this.userInfo
     ) {
