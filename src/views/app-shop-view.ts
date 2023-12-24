1f6812b7a70f0a6cd5ed9c0cd0b43c53f6314de8
PiotrGrzechnik <ppgrzechnik@gmail.com>
2023-12-24 10:34:06 +0100

    app-shop view

--- src/views/app-shop-view.ts
+++ src/views/app-shop-view.ts
@@ -1,4 +1,4 @@
-import { LitElement, PropertyValueMap, css, html } from 'lit';
+import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 import '../components/app-map/app-map';
