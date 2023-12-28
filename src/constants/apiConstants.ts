a7c7c1058d93491a01f4bfc0b9e2f97e7f15b146
PiotrGrzechnik <ppgrzechnik@gmail.com>
2023-12-28 22:07:47 +0100

    Fix api urls

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -1,15 +1,15 @@
 /* USER */
 export const API_USER_INFO = 'User';
 
+/* DOG */
+export const API_DOG_GET = 'Dog';
+export const API_DOG_UPDATE = 'Dog/Update';
+
 /* DOG/DOGHOUSES */
 export const API_DOGHOUSE_GET = 'Dog/Doghouses';
-export const API_DOGHOUSE_CREATE = 'Dog/Doghouses/Create';
-export const API_DOGHOUSE_ATTACK = 'Dog/Doghouses/Attack';
+export const API_DOGHOUSE_CREATE = 'Dog/Doghouse/Create';
+export const API_DOGHOUSE_ATTACK = 'Dog/Doghouse/Attack';
 
 /* DOGHOUSES */
 export const API_DOGHOUSES_NEAR_USER = 'Doghouses/NearUser';
 export const API_DOGHOUSE_UPDATE = 'Doghouses/Update';
-
-/* DOG */
-export const API_DOG_GET = 'Dog';
-export const API_DOG_UPDATE = 'Dog/Update';
