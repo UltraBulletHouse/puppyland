4360669209c7bb12ffdef2b1199e1353fbc6eba7
paweljarec <paweljarec@gmail.com>
2025-08-06 14:49:03 +0200

    add consume all prodcusts button for testing shop

--- src/views/app-shop-view.ts
+++ src/views/app-shop-view.ts
@@ -185,6 +185,10 @@ export class AppShopView extends LitElement {
         font-weight: 700;
         font-size: 16px;
       }
+
+      #consume-all-btn {
+        margin-top: 20px;
+      }
     `,
   ];
 
@@ -208,6 +212,30 @@ export class AppShopView extends LitElement {
     );
 
     return result.data;
+  }
+
+  async consumeAll() {
+    if ('getDigitalGoodsService' in window) {
+      try {
+        const service = await (window as any).getDigitalGoodsService(
+          'https://play.google.com/billing'
+        );
+        const purchases = await service.listPurchases();
+        let consumedCount = 0;
+
+        for (const purchase of purchases) {
+          await service.consume(purchase.purchaseToken);
+          consumedCount++;
+        }
+
+        showSuccessModal(
+          'Consumption Successful',
+          `You have successfully consumed ${consumedCount} purchases.`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   async makePurchase(item: string) {
@@ -342,6 +370,7 @@ export class AppShopView extends LitElement {
                 )}
               `
             : html`<app-spinner></app-spinner>`}
+          <sl-button id="consume-all-btn" @click=${this.consumeAll} pill>Consume All</sl-button>
         </div>
       </div>
     `;
