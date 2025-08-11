94effefb6cb2b73f7d92d21086167ecc73182f20
PiotrGrzechnik <ppgrzechnik@gmail.com>
2025-08-11 20:14:07 +0200

    trigger

--- src/app-index.ts
+++ src/app-index.ts
@@ -150,7 +150,7 @@ export class AppIndex extends LitElement {
   }
 
   render() {
-    /* TEST 2*/
+    /* TEST 3*/
     const isFooterHidden = this.view === View.SIGNIN_VIEW || this.isLoading || !this.view;
 
     const hasShadowFooter = this.view === View.MAP_VIEW;
