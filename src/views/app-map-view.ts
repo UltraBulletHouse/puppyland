c768cc7d00cdbacf57b3b7bf82f074af84d4a991
paweljarec <paweljarec@gmail.com>
2025-10-07 18:27:01 +0200

    walkthrough forced to open on the first enter to map

--- src/views/app-map-view.ts
+++ src/views/app-map-view.ts
@@ -1,4 +1,6 @@
 import { LitElement, css, html } from 'lit';
+import '../components/walkthrough/walkthrough-overlay';
+import { idbGet, idbSet } from '../utils/idb';
 import { customElement, property } from 'lit/decorators.js';
 
 import '../components/app-map/app-map';
@@ -7,6 +9,29 @@ import { Doghouse } from '../types/doghouse';
 
 @customElement('app-map-view')
 export class AppMapView extends LitElement {
+  private _showWalkthrough = false;
+
+  async firstUpdated() {
+    try {
+      const flag = await idbGet<boolean>('hasSeenMapWalkthrough');
+      if (!flag?.value) {
+        this._showWalkthrough = true;
+        this.requestUpdate();
+      }
+    } catch (e) {
+      // Fallback: show once if idb fails
+      this._showWalkthrough = true;
+      this.requestUpdate();
+    }
+  }
+
+  private async _closeWalkthrough() {
+    this._showWalkthrough = false;
+    this.requestUpdate();
+    try {
+      await idbSet('hasSeenMapWalkthrough', true);
+    } catch {}
+  }
   static styles = [
     sharedStyles,
     css`
@@ -24,6 +49,9 @@ export class AppMapView extends LitElement {
     return html`
       <div id="container">
         <app-map .focusTarget=${this.focusTarget}></app-map>
+        ${this._showWalkthrough
+          ? html`<walkthrough-overlay open @close=${this._closeWalkthrough}></walkthrough-overlay>`
+          : null}
       </div>
     `;
   }
